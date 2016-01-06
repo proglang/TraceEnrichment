285fe48bd992bbc0741ae736867c2a050aa76473
jkloos <jkloos@mpi-sws.org>
2016-01-06 18:10:02 +0100

    Export more functions.
    After taking the decision to expose json, a lot of functinos
    can be exported here.

--- trace.mli
+++ trace.mli
@@ -7,6 +7,10 @@ val event_of_string: string -> event
 val objectspec_of_string: string -> objectspec
 val funcspec_of_string: string -> funcspec
 val jsval_of_string: string -> jsval
+val parse_jsval: Yojson.Basic.json -> jsval
+val parse_funcspec: Yojson.Basic.json -> funcspec
+val parse_objectspec: Yojson.Basic.json -> objectspec
+val parse_operation: Yojson.Basic.json -> event
 
 (** [format_tracefile c t] formats a trace file [c] as JSON and writes it to the
   * output channel [c]. *)
@@ -26,3 +30,21 @@ val string_of_rich_event_small : rich_event -> string
 val string_of_points_to : Reference.points_to_map -> string
 
 
+val format_jsval : jsval -> Yojson.Basic.json
+val format_fieldspec : fieldspec -> Yojson.Basic.json
+val format_objectspec : objectspec -> Yojson.Basic.json
+val format_objects : objects -> Yojson.Basic.json
+val format_funcspec : funcspec -> Yojson.Basic.json
+val format_functions : functions -> Yojson.Basic.json
+val format_globals : globals -> Yojson.Basic.json
+val format_objectid : objectid -> Yojson.Basic.json
+val format_fieldref : fieldref -> Yojson.Basic.json
+val format_event : event -> Yojson.Basic.json
+val format_trace : trace -> Yojson.Basic.json
+val format_clean_event : clean_operation -> Yojson.Basic.json
+val format_reference : Reference.reference -> Yojson.Basic.json
+val format_versioned_reference : Reference.versioned_reference -> Yojson.Basic.json
+val format_rich_operation : rich_operation -> Yojson.Basic.json
+val format_rich_event : rich_event -> Yojson.Basic.json
+val format_rich_event_small : rich_event -> Yojson.Basic.json
+val format_points_to : Reference.points_to_map -> Yojson.Basic.json
