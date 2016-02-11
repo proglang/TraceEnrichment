37f71d5fbeb485e7c907b13a115c60628b3be655
jkloos <jkloos@mpi-sws.org>
2016-02-11 15:03:18 +0100

    Build XHR trace server code.

---
+++ traceCollector.ml
@@ -0,0 +1,146 @@
+let (/:) = Filename.concat
+
+let instrument_script_path =
+  List.fold_right (/:) [ "src"; "js"; "commands" ] "instrument.js"
+
+exception InstrumentationError
+
+let instrument filename outdir =
+  let open Lwt in
+  let argarray =
+    [| "node";
+       Config.get_jalangi2_path() /: instrument_script_path;
+       "--inlineIID";
+       "--inlineSource";
+       "--analysis"; Config.get_analysis_script_path ();
+       "--initParam"; "host:" ^ (Config.get_xhr_server_address ());
+       "--initParam"; "port:" ^ (string_of_int (Config.get_xhr_server_port ()));
+       "--outputDir"; outdir;
+       filename |]
+  in match Unix.fork () with
+    | 0 -> Unix.execv (Config.get_node_path ()) argarray
+    | pid ->
+        Lwt_unix.waitpid [] pid
+          >|= function
+            | (_, Unix.WEXITED 0) -> ()
+            | _ -> raise InstrumentationError
+
+let sinks: (string, string -> unit) Hashtbl.t = Hashtbl.create 20
+
+let handle_new make_trace_sink uri body =
+  let%lwt init_json = Cohttp_lwt_body.to_string body in
+  let id = Uuidm.v4_gen (Random.get_state ()) () |> Uuidm.to_string in
+  match make_trace_sink init_json id with
+    | Some sink ->
+          Hashtbl.add sinks id sink;
+          Cohttp_lwt_unix.Server.respond_string ~status:`OK ~body:id ()
+    | None ->
+        Cohttp_lwt_unix.Server.respond_error
+          ~status:`Forbidden ~body:"No trace sinks available" ()
+
+let handle_facts make_trace_sink uri body =
+  let open Lwt in
+  match Uri.get_query_param uri "id" with
+    | Some id ->
+        begin try
+          let sink = Hashtbl.find sinks id in
+            Cohttp_lwt_body.to_string body >>=
+              fun body -> sink body;
+                          Cohttp_lwt_unix.Server.respond_string ~status:`Accepted
+                            ~body:"" ()
+        with
+            Not_found ->
+              Cohttp_lwt_unix.Server.respond_error ~status:`Not_found
+                ~body:("Trace pipeline for " ^ id ^ " not found") ()
+        end
+    | _ ->
+        Cohttp_lwt_unix.Server.respond_error ~status:`Bad_request
+          ~body:"No trace id given" ()
+
+let instrument_tmp_dir = ref None
+
+let get_instrument_tmp_dir () =
+  match !instrument_tmp_dir with
+    | Some dir -> (dir, dir /: "instrumented")
+    | None ->
+        let path = Filename.temp_file "jstools" "" in
+          Unix.mkdir path 0o700;
+          Unix.mkdir (path /: "instrumented") 0o700;
+          instrument_tmp_dir := Some path;
+          (path, path /: "instrumented")
+
+let make_html_wrapper script_name =
+  Fmt.strf
+    "<html><head><script link=\"instrumented/%s\"/></head><body>Running...</body></html>"
+    script_name
+
+let handle_instrument make_trace_sink uri body =
+  let (tmpdir, insdir) = get_instrument_tmp_dir () in
+  let filename = match Uri.get_query_param uri "filename" with
+    | Some filename -> tmpdir /: Filename.basename filename
+    | None -> Filename.temp_file ~temp_dir:tmpdir "gen" ".js"
+  in if Sys.file_exists filename then
+    Cohttp_lwt_unix.Server.respond_error ~status:`Conflict
+      ~body:("JS file called " ^ filename ^ " already exists") ()
+  else
+    let outchan = open_out filename in
+    let%lwt _ = Cohttp_lwt_body.write_body
+                  (fun str -> Lwt.return (output_string outchan str))
+                  body
+    in
+      close_out outchan;
+      let%lwt _ = instrument filename insdir in
+        Cohttp_lwt_unix.Server.respond_string ~status:`OK
+          ~body:(make_html_wrapper (Filename.basename filename)) ()
+
+let trace_connection_switch_tab =
+  [ ("/new", [ (`POST, handle_new) ]);
+    ("/facts", [ (`POST, handle_facts) ]);
+    ("/instrument", [ (`POST, handle_instrument) ])
+  ]
+
+let slash_re = Str.regexp "/"
+
+let trace_collection_handler make_trace_sink conn req body =
+  let open Cohttp in
+  let open Cohttp_lwt_unix in
+  let uri = Request.uri req
+  and meth = Request.meth req in
+    try
+      (* Try the regular handlers first. *)
+      let handlers = List.assoc (Uri.path uri) trace_connection_switch_tab in
+        try
+          (List.assoc meth handlers) make_trace_sink uri body
+        with Not_found -> Server.respond_error ~status:`Method_not_allowed ~body:"" ()
+    with Not_found ->
+      (* Try to find an instrumented script. *)
+      match Str.split slash_re (Uri.path uri) with
+        | [ "instrumented"; filename ] ->
+            let filename = Filename.basename filename in
+            let (_, insdir) = get_instrument_tmp_dir () in
+            let path = insdir /: filename in
+              if Sys.file_exists path then
+                Server.respond_file ~fname:path ()
+              else
+                Server.respond_error ~status:`Not_found
+                  ~body:("Instrumented script " ^ filename ^ " not found") ()
+        | _ -> Server.respond_error ~status:`Not_found ~body:"" ()
+
+let trace_collection_server_impl make_trace_sink =
+  let open Cohttp in
+  let open Cohttp_lwt_unix in
+  let server = Server.make ~callback:(trace_collection_handler make_trace_sink) () in
+  let%lwt conduit_ctx = Conduit_lwt_unix.init ~src:(Config.get_xhr_server_address()) () in
+    Server.create
+      ~ctx:(Cohttp_lwt_unix_net.init ~ctx:conduit_ctx ())
+      ~mode:(`TCP (`Port (Config.get_xhr_server_port())))
+      server
+
+let trace_collection_server make_trace_sink =
+  Lwt.finalize (fun () -> trace_collection_server_impl make_trace_sink)
+    (fun () ->
+       match !instrument_tmp_dir with
+         | None -> Lwt.return ()
+         | Some base ->
+             Lwt.return (FileUtil.rm ~recurse:true ~force:FileUtil.Force [base]))
+
---
+++ traceStream.ml
@@ -0,0 +1,111 @@
+open Trace
+open TraceTypes
+open Types
+
+type items =
+  | ItemFunction of int * funcspec
+  | ItemFunctionUninstrumented of int * string
+  | ItemObject of int * objectspec
+  | ItemStep of event
+  | ItemEnd
+
+exception InvalidItem
+
+let parse_item json =
+  let open Yojson.Basic in
+  match Yojson.Basic.Util.to_list json with
+    | [`String "function"; `Int id; spec] ->
+        ItemFunction (id, parse_funcspec spec)
+    | [`String "object"; `Int id; spec] ->
+        ItemObject (id, parse_objectspec spec)
+    | [`String "step"; json] ->
+        ItemStep (parse_operation json)
+    | [`String "function-uninstrumented"; `Int id; `String code] ->
+        ItemFunctionUninstrumented (id, code)
+    | [`String "end" ] ->
+        ItemEnd
+    | _ ->
+        raise InvalidItem
+
+let rec extract handler = function
+  | x::l ->
+      if handler x then
+        extract handler l
+      else
+        x :: extract handler l
+  | [] -> []
+
+let rec handle_end = function
+  | ItemEnd :: _ -> ([], true)
+  | item :: items -> let (items, at_end) = handle_end items in (item::items, at_end)
+  | [] -> ([], false)
+
+let pad_array value arr length =
+  while BatDynArray.length arr <= length do
+    BatDynArray.add arr value
+  done
+           
+let function_handler initials = function
+  | ItemFunction (id, spec) ->
+      let open Reference in
+        pad_array spec initials.functions id;
+        BatDynArray.set initials.functions id spec;
+        true
+  | _ -> false
+
+let object_handler initials = function
+  | ItemObject (id, spec) ->
+      let open Reference in
+        pad_array spec initials.objects id;
+        BatDynArray.set initials.objects id spec;
+        true
+  | _ -> false
+
+let function_uninstrumented_handler initials = function
+  | ItemFunctionUninstrumented (id, code) ->
+      begin
+        let open Reference in
+          match BatDynArray.get initials.functions id with
+            | Local { from_toString; from_jalangi = None } ->
+                BatDynArray.set initials.functions id
+                  (Local { from_toString; from_jalangi = Some code })
+            | Local { from_toString; from_jalangi = Some code' } ->
+                (* FIXME log an error *)
+                BatDynArray.set initials.functions id
+                  (Local { from_toString; from_jalangi = Some code })
+            | External _ -> raise InvalidItem
+      end; true
+  | _ -> false
+
+let parse_packet initials event_push json_string =
+  let items =
+    Yojson.Basic.from_string json_string
+    |> Yojson.Basic.Util.convert_each parse_item
+  in let (operations, at_end) =
+    items
+    |> extract (function_handler initials)
+    |> extract (function_uninstrumented_handler initials)
+    |> extract (object_handler initials)
+    |> handle_end 
+  in let trace =
+    List.map (function ItemStep op -> op | _ -> failwith "Only ItemStep can happen!")
+      operations
+  in
+    List.iter (fun op -> event_push (Some op)) trace;
+    if at_end then event_push None
+
+let parse_setup_packet json_string =
+  match Yojson.Basic.from_string json_string |> Yojson.Basic.Util.to_list with
+    | [ `Bool globals_are_properties; `Assoc globals_json ] ->
+        let globals = List.fold_left (fun globals (name, val_json) ->
+                                        StringMap.add name (parse_jsval val_json) globals)
+                        StringMap.empty globals_json
+        in let open Reference in
+        let initials =
+          { globals_are_properties; globals;
+            objects = BatDynArray.create ();
+            functions = BatDynArray.create () }
+        in let (stream, push) = Lwt_stream.create ()
+        in (initials, stream, parse_packet initials push)
+    | _ -> raise InvalidItem
+
