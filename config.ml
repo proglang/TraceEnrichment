0e95e922fb424a8821f2c136f053789b298d38b7
jkloos <jkloos@mpi-sws.org>
2016-02-11 15:01:17 +0100

    Support for library configuration.

---
+++ config.ml
@@ -0,0 +1,78 @@
+type 'a argument = 'a option ref
+let node_path: string ref = ref "node"
+let jalangi2_path: string argument = ref None
+let analysis_script_path: string argument = ref None
+let xhr_server_address: string ref = ref "localhost"
+let xhr_server_port: int ref = ref 8888
+let setup_done = ref false
+
+let set_string_arg arg = Arg.String (fun s -> arg := Some s)
+
+let args =
+  let open Arg in
+    [ ("--jalangi", set_string_arg jalangi2_path, "Path to the Jalangi2 installation");
+      ("--analysis", set_string_arg analysis_script_path, "Path to the analysis script");
+      ("--xhr-bind", Set_string xhr_server_address, "Bind the XHR server to this address");
+      ("--xhr-port", Set_int xhr_server_port, "Bind the XHR server to this port");
+      ("--node", Set_string node_path, "Path to the node.js binary") ]
+
+let find_node () =
+  if Sys.command (!node_path ^ " -e 0") == 0 then
+    ()
+  else
+    failwith "No working node installation found"
+
+let (/:) = Filename.concat
+
+let find_jalangi () =
+  let good path = Sys.file_exists (path /: "src" /: "js" /: "commands" /: "instrument.js")
+  in match !jalangi2_path with
+    | Some path ->
+        if good path then () else failwith "No jalangi2 installation found in given path!"
+    | None ->
+        if good "." then
+          jalangi2_path := Some "."
+        else
+          let npm = Filename.dirname !node_path /: "npm" in
+          let chan = Unix.open_process_in (npm ^ " explore jalangi2 -- pwd") in
+            try
+              let path = input_line chan in
+                ignore (Unix.close_process_in chan);
+                if good path then
+                  jalangi2_path := Some path
+                else
+                  failwith "No jalangi2 installation found!"
+            with _ ->
+              ignore (Unix.close_process_in chan);
+              failwith "No jalangi2 installation found!"
+
+let find_analysis_script () =
+  let good path = Sys.file_exists path
+  in match !analysis_script_path with
+    | Some path ->
+        if good path then () else failwith "Analysis script not found in given path!"
+    | None ->
+        let script_base = "xhrtracer.js" in
+          if good script_base then
+            analysis_script_path := Some script_base
+          else
+            let installed_path = CompilationConfig.datadir /: "jstools" /: script_base in
+              if good installed_path then
+                analysis_script_path := Some installed_path
+              else
+                failwith "Analysis script not found!"
+
+let setup () =
+  if not !setup_done then begin
+    find_jalangi();
+    find_analysis_script();
+    setup_done := true
+  end
+
+
+let get_node_path () = !node_path
+let get_jalangi2_path () = setup (); BatOption.get !jalangi2_path
+let get_analysis_script_path () = setup (); BatOption.get !analysis_script_path
+let get_xhr_server_address () = !xhr_server_address
+let get_xhr_server_port () = !xhr_server_port
+
---
+++ config.mli
@@ -0,0 +1,6 @@
+val args : (string * Arg.spec * string) list
+val get_node_path : unit -> string
+val get_jalangi2_path : unit -> string
+val get_analysis_script_path : unit -> string
+val get_xhr_server_address : unit -> string
+val get_xhr_server_port : unit -> int
