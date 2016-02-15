bce21b5e2df5c88492d1aa8c2d995660024f5c1f
jkloos <jkloos@mpi-sws.org>
2016-02-15 20:18:07 +0100

    Provide configuration for the driver HTML page.

--- config.ml
+++ config.ml
@@ -1,7 +1,8 @@
 type 'a argument = 'a option ref
-let node_path: string ref = ref "node"
+let node_path: string ref = ref "/usr/bin/node"
 let jalangi2_path: string argument = ref None
 let analysis_script_path: string argument = ref None
+let analysis_html_path: string argument = ref None
 let xhr_server_address: string ref = ref "localhost"
 let xhr_server_port: int ref = ref 8888
 let setup_done = ref false
@@ -12,6 +13,7 @@ let args =
   let open Arg in
     [ ("--jalangi", set_string_arg jalangi2_path, "Path to the Jalangi2 installation");
       ("--analysis", set_string_arg analysis_script_path, "Path to the analysis script");
+      ("--analysis-html", set_string_arg analysis_html_path, "Path to the analysis HTML driver");
       ("--xhr-bind", Set_string xhr_server_address, "Bind the XHR server to this address");
       ("--xhr-port", Set_int xhr_server_port, "Bind the XHR server to this port");
       ("--node", Set_string node_path, "Path to the node.js binary") ]
@@ -62,10 +64,27 @@ let find_analysis_script () =
               else
                 failwith "Analysis script not found!"
 
+let find_analysis_html () =
+  let good path = Sys.file_exists path
+  in match !analysis_html_path with
+    | Some path ->
+        if good path then () else failwith "Analysis driver not found in given path!"
+    | None ->
+        let script_base = "xhrtracer.html" in
+          if good script_base then
+            analysis_script_path := Some script_base
+          else
+            let installed_path = CompilationConfig.datadir /: "jstools" /: script_base in
+              if good installed_path then
+                analysis_script_path := Some installed_path
+              else
+                failwith "Analysis driver not found!"
+
 let setup () =
   if not !setup_done then begin
     find_jalangi();
     find_analysis_script();
+    find_analysis_html();
     setup_done := true
   end
 
@@ -73,6 +92,7 @@ let setup () =
 let get_node_path () = !node_path
 let get_jalangi2_path () = setup (); BatOption.get !jalangi2_path
 let get_analysis_script_path () = setup (); BatOption.get !analysis_script_path
+let get_analysis_html_path () = setup(); BatOption.get !analysis_html_path
 let get_xhr_server_address () = !xhr_server_address
 let get_xhr_server_port () = !xhr_server_port
 
