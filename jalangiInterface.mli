1a33292a0ca1cbfb9e706f4415b4e445a6466d60
jkloos <jkloos@mpi-sws.org>
2016-02-15 20:19:40 +0100

    Proper instrumentation interface.

---
+++ jalangiInterface.mli
@@ -0,0 +1,30 @@
+(** Interface with Jalangi2. *)
+(** Instrumentation failed. *)
+exception InstrumentationError
+(** Perform instrumentation.
+   
+  [jalangi2_instrument strategy filenames outdir] instruments the
+  files in [filenames] using the generic trace analysis, using
+  trace output strategy [strategy], and puts the results into
+  [outdir]. *)
+val jalangi2_instrument : string -> string list -> string -> unit Lwt.t
+(**
+  A complete solution for instrumenting a JavaScript file for browser-based
+  trace generation.
+
+  [basename] gives a base name for the instrumented JavaScript file and
+  the HTML page that drives the analysis.
+
+  [providejs] gets called as [providejs jsfile], where [jsfile] is the proposed
+  filename for the uninstrumented JavaScript file. If it returns [None], the
+  uninstrumented JavaScript file must be provided in [jsfile], and if it is
+  [Some path], it must be provided in [path].
+
+  The function performs the instrumentation and returns the base name of
+  instrumented JavaScript file and driver page, i.e., if it returns
+  "/path/to/xyz", then the instrumented JS file can be found in
+  "/path/to/xyz.js" and the driver page in "/path/to/xyz.html".
+  *)
+val instrument_for_browser :
+  ?basename:string ->
+  providejs:(string -> string option Lwt.t) -> string Lwt.t
