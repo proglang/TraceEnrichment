4e3b78eb1fc3320be1c6f4fe1ee444faf948b4a5
jkloos <jkloos@mpi-sws.org>
2016-02-15 20:19:58 +0100

    Tests for JalangiInterface

---
+++ jalangiInterfaceTest.ml
@@ -0,0 +1,23 @@
+open Kaputt.Abbreviations
+
+let (|>) = Pervasives.(|>)
+let same_file expected got =
+  Assert.make_equal (=) Odiff.string_of_diffs
+    [] (Odiff.strings_diffs expected got)
+
+let test_jalangi_instrument =
+  Test.make_assert_test ~title:"Instrumenting a JavaScript file, simple"
+    (fun () -> Lwt_main.run (Files.temp_dir_lwt "test" ""))
+    (fun tmpdir ->
+       JalangiInterface.jalangi2_instrument "xhr"
+         [ "testdata/test-instrument.js"; "testdata/test-instrument.html" ]
+         tmpdir
+       |> Lwt_main.run |> ignore;
+       same_file "testdata/test-instrumented.instrumented.js"
+         (tmpdir ^ "/test-instrument.js");
+       same_file "testdata/test-instrumented.instrumented.html"
+         (tmpdir ^ "/test-instrument.html");
+       tmpdir)
+    (fun tmpdir -> FileUtil.rm ~force:FileUtil.Force ~recurse:true [tmpdir])
+
+let _ = Test.run_tests [ test_jalangi_instrument ]
---
+++ testdata/test-instrument.instrumented.js
@@ -0,0 +1,19 @@
+J$.iids = {"nBranches":2,"originalCodeFileName":"out/test-instrument_orig_.js","instrumentedCodeFileName":"out/test-instrument.js"};
+jalangiLabel0:
+    while (true) {
+        try {
+            J$.Se(41, 'out/test-instrument.js', 'out/test-instrument_orig_.js');
+            J$.X1(33, J$.M(25, J$.R(9, 'console', console, 2), 'log', 0)(J$.T(17, 1, 22, false)));
+        } catch (J$e) {
+            J$.Ex(49, J$e);
+        } finally {
+            if (J$.Sr(57)) {
+                J$.L();
+                continue jalangiLabel0;
+            } else {
+                J$.L();
+                break jalangiLabel0;
+            }
+        }
+    }
+// JALANGI DO NOT INSTRUMENT
