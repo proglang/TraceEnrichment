753f251f14bdb8b53ab22d684092d42e7f4f73bb
jkloos <jkloos@mpi-sws.org>
2016-02-16 21:22:58 +0100

    Rewrite of the trace collector.

--- traceCollector.ml
+++ traceCollector.ml
@@ -1,170 +1,149 @@
-let (/:) = Filename.concat
+(** HTTP reply functions. *)
+let reply_text ?(status=`OK) content body =
+  let headers = Cohttp.Header.init_with "Content-Type" content in
+    Cohttp_lwt_unix.Server.respond_string ~headers ~status ~body ()
+let reply_plain_text ?(status=`OK) body = reply_text ~status "text/plain" body
+let reply_json_text ?(status=`OK) body = reply_text ~status "application/json" body
+let reply_html ?(status=`OK) body = reply_text ~status "text/html" body
+let reply_javascript ?(status=`OK) body = reply_text ~status "application/javascript" body
+let reply_error status body =
+  let headers = Cohttp.Header.init_with "Content-Type" "text/plain" in
+    Cohttp_lwt_unix.Server.respond_error ~status ~body ~headers ()
+let reply_file content filename =
+  let headers = Cohttp.Header.init_with "Content-Type" content in
+    Cohttp_lwt_unix.Server.respond_file ~headers ~fname:filename ()
 
-let instrument_script_path =
-  List.fold_right (/:) [ "src"; "js"; "commands" ] "generic-tracer.js"
+let make_new_uuid () =
+  Uuidm.v4_gen (Random.get_state ()) ()
+    |> Uuidm.to_string
 
-exception InstrumentationError
+type handler =
+    Uri.t -> string Lwt.t -> (Cohttp.Response.t * Cohttp_lwt_body.t) Lwt.t
 
-let instrument filename outdir =
-  let open Lwt in
-  let argarray =
-    [| "node";
-       Config.get_jalangi2_path() /: instrument_script_path;
-       "--inlineIID";
-       "--inlineSource";
-       "--analysis"; Config.get_analysis_script_path ();
-       "--initParam"; "host:" ^ (Config.get_xhr_server_address ());
-       "--initParam"; "port:" ^ (string_of_int (Config.get_xhr_server_port ()));
-       "--initParam"; "strategy:xhr";
-       "--outputDir"; outdir;
-       filename |]
-  in match Unix.fork () with
-    | 0 -> Unix.execv (Config.get_node_path ()) argarray
-    | pid ->
-        Lwt_unix.waitpid [] pid
-          >|= function
-            | (_, Unix.WEXITED 0) -> ()
-            | _ -> raise InstrumentationError
+module type STRATEGY = sig
+  val make_trace_sink:
+    init_data:string ->
+    id:string ->
+    finish:(unit -> unit) ->
+    (string -> unit) Lwt.t
+  val handlers_global: ((string * Cohttp.Code.meth) * handler) list
+  val handlers_local: ((string * Cohttp.Code.meth) * (string -> handler)) list
+end
 
-let sinks: (string, string -> unit) Hashtbl.t = Hashtbl.create 20
+module Server(S: STRATEGY) = struct
+  open Cohttp.Code
 
-let handle_new make_trace_sink uri body =
-  let%lwt init_json = Cohttp_lwt_body.to_string body in
-  let id = Uuidm.v4_gen (Random.get_state ()) () |> Uuidm.to_string in
-  match%lwt make_trace_sink ~init_data:init_json ~id:id with
-    | Some sink ->
-          Hashtbl.add sinks id sink;
-          Cohttp_lwt_unix.Server.respond_string ~status:`OK ~body:id ()
-    | None ->
-        Cohttp_lwt_unix.Server.respond_error
-          ~status:`Forbidden ~body:"No trace sinks available" ()
+  let sessions = Hashtbl.create 20
+  let shutdown = ref false
 
-let handle_facts make_trace_sink uri body =
-  let open Lwt in
-  match Uri.get_query_param uri "id" with
-    | Some id ->
-        begin try
-          let sink = Hashtbl.find sinks id in
-            Cohttp_lwt_body.to_string body >>=
-              fun body -> sink body;
-                          Cohttp_lwt_unix.Server.respond_string ~status:`Accepted
-                            ~body:"" ()
-        with
-            Not_found ->
-              Cohttp_lwt_unix.Server.respond_error ~status:`Not_found
-                ~body:("Trace pipeline for " ^ id ^ " not found") ()
-        end
-    | _ ->
-        Cohttp_lwt_unix.Server.respond_error ~status:`Bad_request
-          ~body:"No trace id given" ()
-
-let instrument_tmp_dir = ref None
-
-let get_instrument_tmp_dir () =
-  match !instrument_tmp_dir with
-    | Some dir -> (dir, dir /: "instrumented")
-    | None ->
-        let path = Filename.temp_file "jstools" "" in
-          Unix.mkdir path 0o700;
-          Unix.mkdir (path /: "instrumented") 0o700;
-          instrument_tmp_dir := Some path;
-          (path, path /: "instrumented")
-
-let make_html_wrapper script_name =
-  Fmt.strf
-    "<html><head><script link=\"instrumented/%s\"/></head><body>Running...</body></html>"
-    script_name
-
-let handle_instrument make_trace_sink uri body =
-  let (tmpdir, insdir) = get_instrument_tmp_dir () in
-  let filename = match Uri.get_query_param uri "filename" with
-    | Some filename -> tmpdir /: Filename.basename filename
-    | None -> Filename.temp_file ~temp_dir:tmpdir "gen" ".js"
-  in if Sys.file_exists filename then
-    Cohttp_lwt_unix.Server.respond_error ~status:`Conflict
-      ~body:("JS file called " ^ filename ^ " already exists") ()
-  else
-    let outchan = open_out filename in
-    let%lwt _ = Cohttp_lwt_body.write_body
-                  (fun str -> Lwt.return (output_string outchan str))
-                  body
+  let handler_new uri body =
+    let id = make_new_uuid () in
+    let%lwt init_data = body in
+    let%lwt sink =
+      S.make_trace_sink ~init_data ~id ~finish:(fun () -> shutdown := true)
     in
-      close_out outchan;
-      let%lwt _ = instrument filename insdir in
-        Cohttp_lwt_unix.Server.respond_string ~status:`OK
-          ~body:(make_html_wrapper (Filename.basename filename)) ()
+      Hashtbl.add sessions id sink;
+      reply_plain_text id
 
-let trace_connection_switch_tab =
-  [ ("/new", [ (`POST, handle_new) ]);
-    ("/facts", [ (`POST, handle_facts) ]);
-    ("/instrument", [ (`POST, handle_instrument) ])
-  ]
+  let handler_instrument uri body =
+    try 
+      let uri = Uri.path uri in
+      let basename =
+        if uri = "" then None else Some (Filename.basename uri)
+      in let%lwt base =
+        JalangiInterface.instrument_for_browser ?basename
+          ~providejs:(fun path ->
+                        Lwt_io.with_file ~mode:Lwt_io.Output path
+                          (fun channel -> Lwt.bind body (Lwt_io.write channel)))
+      in reply_plain_text base
+    with Exit -> reply_error `Not_found "No such handler"
 
-let slash_re = Str.regexp "/"
+  let handler_shutdown uri body =
+    shutdown := true;
+    reply_plain_text "Shuting down"
 
-let trace_collection_handler make_trace_sink conn req body =
-  let open Cohttp in
-  let open Cohttp_lwt_unix in
-  let uri = Request.uri req
-  and meth = Request.meth req in
-    try
-      (* Try the regular handlers first. *)
-      let handlers = List.assoc (Uri.path uri) trace_connection_switch_tab in
-        try
-          (List.assoc meth handlers) make_trace_sink uri body
-        with Not_found -> Server.respond_error ~status:`Method_not_allowed ~body:"" ()
-    with Not_found ->
-      (* Try to find an instrumented script. *)
-      match Str.split slash_re (Uri.path uri) with
-        | [ "instrumented"; filename ] ->
-            let filename = Filename.basename filename in
-            let (_, insdir) = get_instrument_tmp_dir () in
-            let path = insdir /: filename in
-              if Sys.file_exists path then
-                Server.respond_file ~fname:path ()
-              else
-                Server.respond_error ~status:`Not_found
-                  ~body:("Instrumented script " ^ filename ^ " not found") ()
-        | _ -> Server.respond_error ~status:`Not_found ~body:"" ()
+  let handler_facts id uri body =
+    if Uri.path uri = "" then begin
+      let%lwt body = body in
+      Hashtbl.find sessions id body;
+      reply_plain_text ~status:`Accepted "Accepted for processing"
+    end else
+      reply_error `Not_found "No such handler"
 
-let trace_collection_server_impl make_trace_sink =
-  let open Cohttp in
-  let open Cohttp_lwt_unix in
-  let server = Server.make ~callback:(trace_collection_handler make_trace_sink) () in
-  let%lwt conduit_ctx = Conduit_lwt_unix.init ~src:(Config.get_xhr_server_address()) () in
-    Server.create
-      ~ctx:(Cohttp_lwt_unix_net.init ~ctx:conduit_ctx ())
-      ~mode:(`TCP (`Port (Config.get_xhr_server_port())))
-      server
+  let handler_index query =
+    reply_plain_text "No implementation so far"
 
-let trace_collection_server make_trace_sink =
-  Lwt.finalize (fun () -> trace_collection_server_impl make_trace_sink)
-    (fun () ->
-       match !instrument_tmp_dir with
-         | None -> Lwt.return ()
-         | Some base ->
-             Lwt.return (FileUtil.rm ~recurse:true ~force:FileUtil.Force [base]))
+  let handle_session_management id meth =
+    match meth with
+      | `DELETE ->
+          if Hashtbl.mem sessions id then begin
+            let sink = Hashtbl.find sessions id in
+              Hashtbl.remove sessions id;
+              begin try sink "[['end']]" with _ -> () end;
+              reply_plain_text "removed"
+          end else
+            reply_error `Not_found "No such resource"
+      | _ ->
+          reply_error `Method_not_allowed "Cannot access using this method"
 
-let one_shot_server trace_consumer =
-  let result = Lwt_mvar.create_empty ()
-  and waiting = ref true in
+  let handlers_global =
+    let open Cohttp.Code in
+    (("new", `POST), handler_new) ::
+    (("instrument", `POST), handler_instrument) ::
+    (("shutdown", `POST), handler_shutdown) ::
+    S.handlers_global
+
+  let handlers_local =
+    (("facts", `POST), handler_facts) ::
+    S.handlers_local
+
+  let colon_re = Str.regexp ":"
+  let html_filename_re = Str.regexp "^[-a-zA-Z0-9._]*\\.html$"
+  let js_filename_re = Str.regexp "^[-a-zA-Z0-9._]*\\.js$"
+
+  let multiplex conn req body =
+    let open Cohttp.Request in
+    let uri = uri req
+    and body = Cohttp_lwt_body.to_string body
+    and meth = meth req
+    in let update_uri tail = Uri.with_path uri (BatString.concat "/" tail)
+    in match Str.split colon_re (Uri.path uri) with
+        | [] ->
+            if meth = `GET then
+              handler_index (Uri.query uri)
+            else
+              reply_error `Method_not_allowed ("Can't access / using this method")
+        | [id] when Hashtbl.mem sessions id ->
+            handle_session_management id meth
+        | id::op::tail when Hashtbl.mem sessions id ->
+            begin try
+              List.assoc (op, meth) handlers_local id (update_uri tail) body
+            with Not_found ->
+              reply_error `Not_found ("No handler found")
+            end
+        | op::tail ->
+            begin try
+              List.assoc (op, meth) handlers_global (update_uri tail) body
+            with Not_found ->
+              let%lwt insdir = JalangiInterface.get_instrumented_dir () in
+              let path = Filename.concat insdir op in
+              if tail = [] && Sys.file_exists path then begin
+                if Str.string_match html_filename_re op 0 then
+                  reply_file "text/html" path
+                else if Str.string_match js_filename_re op 0 then
+                  reply_file "application/javascript" path
+                else
+                  reply_error `Forbidden "Cannot access this file"
+              end else
+                reply_error `Not_found "No handler found"
+            end
+
   let server =
-    trace_collection_server
-      (fun ~init_data ~id ->
-         if !waiting then begin
-           waiting := false;
-           let (initials, stream, push) =
-             TraceStream.parse_setup_packet init_data in
-           let%lwt _ = Lwt_mvar.put result (trace_consumer initials stream) in
-             Lwt.return_some push
-         end else
-           Lwt.return_none)
-  in Lwt.ignore_result server; Lwt.bind (Lwt_mvar.take result) (fun x -> x)
+    let server = Cohttp_lwt_unix.Server.make ~callback:multiplex () in
+    let%lwt ctx = Conduit_lwt_unix.init ~src:(Config.get_xhr_server_address ()) () in
+      Cohttp_lwt_unix.Server.create
+        ~ctx:(Cohttp_lwt_unix_net.init ~ctx ())
+        ~mode:(`TCP (`Port (Config.get_xhr_server_port ())))
+        server
 
-let generic_server trace_consumer =
-  trace_collection_server (fun ~init_data ~id ->
-                             let (initials, stream, push) =
-                               TraceStream.parse_setup_packet init_data
-                             in (trace_consumer id initials stream: unit);
-                                Lwt.return_some push)
-
+end
