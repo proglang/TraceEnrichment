15f961b08506241d2cb289b532fa9f19562421f4
jkloos <jkloos@mpi-sws.org>
2016-02-16 11:38:43 +0100

    Drop dependency on ocamldiff.

--- jalangiInterfaceTest.ml
+++ jalangiInterfaceTest.ml
@@ -1,9 +1,16 @@
 open Kaputt.Abbreviations
 
 let (|>) = Pervasives.(|>)
+let tmppath_re = Str.regexp "/tmp/test[0-9a-fA-F]*/"
+
 let same_file expected got =
-  Assert.make_equal (=) Odiff.string_of_diffs
-    [] (Odiff.files_diffs expected got)
+  (* Normalize the ``got'' file a bit. *)
+  let got' = Filename.temp_file "result" "" in
+  let lines = BatFile.lines_of got in
+  let lines' = BatEnum.map (Str.global_replace tmppath_re "out/") lines in
+    BatFile.write_lines got' lines';
+  Assert.equal_int ~msg:"Diffing files, return value" 0
+    (Sys.command ("diff -u -w -B " ^ expected ^ " " ^ got'))
 
 let test_jalangi_instrument =
   Test.make_assert_test ~title:"Instrumenting a JavaScript file, simple"
