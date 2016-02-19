e47bce4eca1e3d01ac97f000ef73b1649720690a
jkloos <jkloos@mpi-sws.org>
2016-02-19 17:44:57 +0100

    Better debugging.
    Also,. some clean-up.

--- traceCollector.ml
+++ traceCollector.ml
@@ -41,6 +41,12 @@ module Server(S: STRATEGY) = struct
   let log_warn = BatLogger.log logger BatLogger.WARN
   let log_error = BatLogger.log logger BatLogger.ERROR
   let log_fatal = BatLogger.log logger BatLogger.FATAL
+  let debug str = log_debug (fun () -> (str, []))
+  let info str = log_info (fun () -> (str, []))
+  let notice str = log_notice (fun () -> (str, []))
+  let warn str = log_warn (fun () -> (str, []))
+  let error str = log_error (fun () -> (str, []))
+  let fatal str = log_fatal (fun () -> (str, []))
 
   let (stop, wakener) = Lwt.wait ()
   let shutdown: unit -> unit = Lwt.wakeup wakener
@@ -48,6 +54,7 @@ module Server(S: STRATEGY) = struct
   let handler_new uri body =
     let id = make_new_uuid () in
     let%lwt init_data = body in
+    log_info (fun () -> ("Adding new handler", [("id", id); ("body", init_data)]));
     let%lwt sink =
       S.make_trace_sink ~init_data ~id ~finish:shutdown
     in
@@ -59,13 +66,21 @@ module Server(S: STRATEGY) = struct
       let uri = Uri.path uri in
       let basename =
         if uri = "" then None else Some (Filename.basename uri)
-      in let%lwt base =
+      in
+        log_info (fun () -> ("Instrumenting", match basename with Some bn -> ["basename",bn] | _ -> []));
+      let%lwt base =
         JalangiInterface.instrument_for_browser ?basename
           ~providejs:(fun path ->
+                        log_debug (fun () -> ("Writing JS code", ["path", path]));
                         Lwt_io.with_file ~mode:Lwt_io.Output path
                           (fun channel -> Lwt.bind body (Lwt_io.write channel)))
-      in reply_plain_text (Filename.basename base)
-    with Exit -> reply_error `Not_found "No such handler"
+      in
+        log_info (fun () -> ("Performed instrumentation", ["base", base]));
+        reply_plain_text (Filename.basename base)
+    with
+        e -> log_warn (fun () -> ("Exception in instrumentation", [("exc", Printexc.to_string e)]));
+             reply_error `Internal_server_error (Printexc.to_string e)
+
 
   let handler_shutdown uri body =
     log_info (fun () -> ("Shutting down server", []));
@@ -76,25 +91,31 @@ module Server(S: STRATEGY) = struct
   let handler_facts id uri body =
     if Uri.path uri = "" then begin
       let%lwt body = body in
+      log_info (fun () -> ("Feeding facts", [("id", id); ("data", body)]));
       Hashtbl.find sessions id body;
       reply_plain_text ~status:`Accepted "Accepted for processing"
-    end else
+    end else begin
+      info "Fact handling with extra arguments";
       reply_error `Not_found "No such handler"
+    end
 
   let handler_index query =
+    warn "Unimplemented index functionality";
     reply_plain_text "No implementation so far"
 
   let handle_session_management id meth =
     match meth with
       | `DELETE ->
+          log_info (fun () -> ("Deleting session", ["id", id]));
           if Hashtbl.mem sessions id then begin
             let sink = Hashtbl.find sessions id in
               Hashtbl.remove sessions id;
-              begin try sink "[['end']]" with _ -> () end;
+              begin try sink "[[\"end\"]]" with _ -> () end;
               reply_plain_text "removed"
           end else
             reply_error `Not_found "No such resource"
       | _ ->
+          info "Session management with bad method";
           reply_error `Method_not_allowed "Cannot access using this method"
 
   let handlers_global =
@@ -113,65 +134,68 @@ module Server(S: STRATEGY) = struct
   let js_filename_re = Str.regexp "^[-a-zA-Z0-9._]*\\.js$"
 
   let multiplex conn req body =
-    let open Cohttp.Request in
-    let uri = uri req
-    and body = Cohttp_lwt_body.to_string body
-    and meth = meth req
-    in log_info (fun () -> ("Received request", [("URI", Uri.to_string uri); ("Method", Cohttp.Code.string_of_method meth)]));
-    let update_uri tail = Uri.with_path uri (BatString.concat "/" tail)
-    in let result = match Str.split slash_re (BatString.strip ~chars:"/" (Uri.path uri)) with
-        | [] ->
-            log_debug (fun () -> ("Index page handling", []));
-            if meth = `GET then
-              handler_index (Uri.query uri)
-            else
-              reply_error `Method_not_allowed ("Can't access / using this method")
-        | [id] when Hashtbl.mem sessions id ->
-            log_debug (fun () -> ("Session management", []));
-            handle_session_management id meth
-        | id::op::tail when Hashtbl.mem sessions id ->
-            begin try
-              log_debug (fun () -> ("Per-session operations", [("session", id); ("operation", op)]));
-              List.assoc (op, meth) handlers_local id (update_uri tail) body
-            with Not_found ->
-              log_info (fun () -> ("No handler found", ["session", id; "operation", op]));
-              reply_error `Not_found ("No handler found")
-            end
-        | op::tail ->
-            log_debug (fun () -> ("op: not a handler", ["op", op] @ Hashtbl.fold (fun key _ keys -> ("key", key) :: keys) sessions []));
-            begin try
-              log_debug (fun () -> ("Global operation", [("operation", op)]));
-              List.assoc (op, meth) handlers_global (update_uri tail) body
-            with Not_found ->
-              log_debug (fun () -> ("No handler found, trying file handling", []));
-              let%lwt insdir = JalangiInterface.get_instrumented_dir () in
-              let path = Filename.concat insdir op in
-              if tail = [] && Sys.file_exists path then begin
-                log_debug (fun () -> ("Found file, checking if permissible to send", []));
-                if Str.string_match html_filename_re op 0 then
-                  reply_file "text/html" path
-                else if Str.string_match js_filename_re op 0 then
-                  reply_file "application/javascript" path
-                else
-                  reply_error `Forbidden "Cannot access this file"
-              end else
-                reply_error `Not_found "No handler found"
-            end
-    in log_debug (fun () -> ("Made reply", [])); result
+    try
+      let open Cohttp.Request in
+      let uri = uri req
+      and body = Cohttp_lwt_body.to_string body
+      and meth = meth req
+      in log_info (fun () -> ("Received request", [("URI", Uri.to_string uri); ("Method", Cohttp.Code.string_of_method meth)]));
+         let update_uri tail = Uri.with_path uri (BatString.concat "/" tail)
+         in
+           match Str.split slash_re (BatString.strip ~chars:"/" (Uri.path uri)) with
+             | [] ->
+                 if meth = `GET then
+                   handler_index (Uri.query uri)
+                 else begin
+                   info "Accessing index with bad method";
+                   reply_error `Method_not_allowed ("Can't access / using this method")
+                 end
+             | [id] when Hashtbl.mem sessions id ->
+                 handle_session_management id meth
+             | id::op::tail when Hashtbl.mem sessions id ->
+                 begin try
+                   List.assoc (op, meth) handlers_local id (update_uri tail) body
+                 with Not_found ->
+                   log_info (fun () -> ("No handler found", ["session", id; "operation", op]));
+                   reply_error `Not_found ("No handler found")
+                 end
+             | op::tail ->
+                 begin try
+                   log_debug (fun () -> ("Global operation", [("operation", op)]));
+                   List.assoc (op, meth) handlers_global (update_uri tail) body
+                 with Not_found ->
+                   log_info (fun () -> ("No handler found, trying file handling", []));
+                   let%lwt insdir = JalangiInterface.get_instrumented_dir () in
+                   let path = Filename.concat insdir op in
+                     if tail = [] && Sys.file_exists path then begin
+                       if Str.string_match html_filename_re op 0 then begin
+                         info "Serving HTML file";
+                         reply_file "text/html" path
+                       end else if Str.string_match js_filename_re op 0 then begin
+                         info "Serving JS file";
+                         reply_file "application/javascript" path
+                       end else
+                         reply_error `Forbidden "Cannot access this file"
+                     end else
+                       reply_error `Not_found "No handler found"
+                 end
+    with 
+      | e -> log_error (fun () -> (Printexc.to_string e, [])); raise e
 
   let server =
     let open Lwt in
-    BatLogger.init [("JSCollector", BatLogger.DEBUG)] BatLogger.stderr_formatter;
-    log_debug (fun () -> ("Starting JSCollector server.", []));
-    let server = Cohttp_lwt_unix.Server.make ~callback:multiplex () in
-      log_debug (fun () -> ("Created server instance.", []));
-    let%lwt ctx = Conduit_lwt_unix.init ~src:(Config.get_xhr_server_address ()) () in
-      log_debug (fun () -> ("Set up binding address, starting...", []));
-      Cohttp_lwt_unix.Server.create
-        ~stop
-        ~ctx:(Cohttp_lwt_unix_net.init ~ctx ())
-        ~mode:(`TCP (`Port (Config.get_xhr_server_port ())))
-        server
-                >|= fun () -> log_info (fun () -> ("Server shut down", []))
+      BatLogger.init [("JSCollector", BatLogger.DEBUG)] BatLogger.stderr_formatter;
+      log_debug (fun () -> ("Starting JSCollector server.", []));
+      let server = Cohttp_lwt_unix.Server.make ~callback:multiplex () in
+      let%lwt ctx = Conduit_lwt_unix.init ~src:(Config.get_xhr_server_address ()) () in
+      let%lwt () =
+        Cohttp_lwt_unix.Server.create
+          ~stop
+          ~ctx:(Cohttp_lwt_unix_net.init ~ctx ())
+          ~mode:(`TCP (`Port (Config.get_xhr_server_port ())))
+          server
+      in log_info (fun () -> ("Server shut down", []));
+         JalangiInterface.clean_up();
+         Lwt.return_none
 
 end
