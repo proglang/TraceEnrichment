af4560895625ba07d89c4a537b9f15b729e3a4fa
jkloos <jkloos@mpi-sws.org>
2016-02-19 18:39:17 +0100

    Testing for trace stream servers.

---
+++ traceStreamServerTest.ml
@@ -0,0 +1,31 @@
+module TestStreamStrategy: TraceStreamServer.STREAMSTRATEGY = struct
+  open Lwt
+  type t = string list
+  let stream_setup id initials raw_stream =
+    Lwt_stream.to_list raw_stream >|=
+      List.map (fun ev -> Fmt.to_to_string TraceTypes.pp_operation ev)
+
+  let stream_dump (strings: t) _ _ =
+    TraceCollector.reply_plain_text (BatString.concat "\n" strings)
+
+  let display_menu _ _ =
+    let headers = Cohttp.Header.init_with "Content-Type" "text/html" in
+    Cohttp_lwt_unix.Server.respond_file ~headers ~fname:"menu.html" ()
+
+  let handlers_global = [
+    ("menu", let open Cohttp.Code in `GET),
+    display_menu
+  ]
+  let handlers_local = [
+    ("pretty", let open Cohttp.Code in `GET),
+    stream_dump
+  ]
+end
+
+module Server = TraceStreamServer.TraceStreamServer(TestStreamStrategy)
+
+let () =
+  Arg.parse Config.args (fun _ -> failwith "Unexpected argument")
+    "traceStreamServerTest [options]";
+  Server.run_server ()
+
