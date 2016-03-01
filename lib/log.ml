91364334e140592d15e71b780c679b3d72b68900
jkloos <jkloos@mpi-sws.org>
2016-03-01 18:53:39 +0100

    Set log console width.

--- lib/log.ml
+++ lib/log.ml
@@ -2,6 +2,8 @@ let src = Logs.Src.create ~doc:"JSEnrichment library" "jsenrichment"
 module Log = (val Logs.src_log src: Logs.LOG)
 include Log
 let default_setup debugging_mode =
-  if debugging_mode then
+  if debugging_mode then begin
     Logs.set_level ~all:true (Some Logs.Debug);
+    Format.set_margin 132
+  end;
   Logs.set_reporter (Logs_fmt.reporter ())
