2ec51897047f967fba3f24237bbe31179e1482d2
jkloos <jkloos@mpi-sws.org>
2016-03-08 11:13:58 +0100

    Add interface for TraceStreamServer.

---
+++ xhr/traceStreamServer.mli
@@ -0,0 +1,16 @@
+module type STREAMSTRATEGY =
+  sig
+    type t
+    val stream_setup : string -> Types.initials -> TraceTypes.raw_stream -> t
+    val handlers_global :
+      ((string * Cohttp.Code.meth) * (string * TraceCollector.handler)) list
+    val handlers_local :
+      ((string * Cohttp.Code.meth) * (string * (t -> TraceCollector.handler)))
+      list
+  end
+module type TSS =
+  sig
+    val server : unit Lwt.t
+    val run_server : unit -> unit
+  end
+module TraceStreamServer : functor (S : STREAMSTRATEGY) -> TSS
