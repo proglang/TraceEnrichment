0240aa1497940ff7c215d05002918ae6b768f016
jkloos <jkloos@mpi-sws.org>
2016-03-09 17:25:46 +0100

    Create a benchmarking helper.
    This is pprichtrace without the output part;
    it can be used to benchmark the algorithms.

---
+++ tools/richtrace_bench.ml
@@ -0,0 +1,17 @@
+let () =
+  let files = ref []
+  and debug = ref false in
+    Arg.parse
+      [("-D", Arg.Set debug, "Debugging mode");
+       ("-V", Arg.Unit Debug.enable_validate, "Enable validation");
+       ("-f", Arg.Unit TraceTypes.enable_dump_facts, "Dump local facts")
+      ]
+      (fun file -> files := !files @ [file])
+      "ppcleantrace [-D] [-V] [-f] files";
+    Log.default_setup !debug;
+    List.iter (fun file -> file
+                 |> open_in
+                 |> Trace.parse_tracefile
+                 |> RichTrace.tracefile_to_rich_tracefile
+                 |> ignore)
+      !files
