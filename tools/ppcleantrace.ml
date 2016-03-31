7be4e41b30ceba4aeb90079c3ca324ca73315262
jkloos <jkloos@mpi-sws.org>
2016-03-31 16:49:30 +0200

    Expose trace cleanup phases.

--- tools/ppcleantrace.ml
+++ tools/ppcleantrace.ml
@@ -1,9 +1,14 @@
 let () =
+  let level = ref CleanTrace.SynthesizeEvents in
   let files = ref []
   and debug = ref false in
     Arg.parse
       [("-D", Arg.Set debug, "Debugging mode");
        ("-V", Arg.Unit Debug.enable_validate, "Enable validation");
+       ("-1", Arg.Unit (fun () -> level := CleanTrace.JustClean), "Only clean, no normalization");
+       ("-2", Arg.Unit (fun () -> level := CleanTrace.Normalizations), "No call or getter/setter normalization");
+       ("-3", Arg.Unit (fun () -> level := CleanTrace.SynthesizeGettersAndSetters), "No call normalization");
+       ("-4", Arg.Unit (fun () -> level := CleanTrace.SynthesizeEvents), "Full normalization");
       ]
       (fun file -> files := !files @ [file])
       "ppcleantrace [-D] [-V] files";
